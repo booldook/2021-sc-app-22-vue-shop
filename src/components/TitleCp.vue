1a207021adb1e041905cb82d0ce73a03411167be
booldook <booldook@gmail.com>
2021-10-05 16:00:14 +0900

    Lint 적용

---
+++ src/components/TitleCp.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header-wrapper">
+    <h1 class="logo-wrap">
+      <img src="@/assets/img/logo.png" alt="logo" class="logo">
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TitleCp',
+  props: ['title'],
+};
+</script>
+
+<style lang="scss" scoped>
+.header-wrapper {
+  padding: $gutter-width;
+  background-color: $secondary-color;
+  color: $primary-color;
+  font-size: 1.5em;
+  .logo {
+    width: 1.5em;
+  }
+}
+</style>
